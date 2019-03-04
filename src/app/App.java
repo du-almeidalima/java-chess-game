9cb1eec52e95202548549b9dfd496e6c6e6f8878
Xenosgrilda <dualmeidalima@gmail.com>
2019-03-04 15:44:53 -0300

    Position Class

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,17 @@
+/**
+ * @author Eduardo Lima
+ */
+
 package app;
+
+import models.boardgame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        // Variables
+        Position pos;
+
+        pos = new Position(10, 5);
+        System.out.println(pos);
     }
 }
---
+++ src/models/boardgame/Position.java
@@ -0,0 +1,33 @@
+package models.boardgame;
+
+public class Position{
+    private Integer row;
+    private Integer col;
+
+    public Position(Integer row, Integer col){
+        this.row = row;
+        this.col = col;
+    }
+
+    // Getters and Setters
+
+    public Integer getCol() {
+        return col;
+    }
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setCol(Integer col) {
+        this.col = col;
+    }
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Row: " + this.getCol() + " | Col: " + this.getCol();
+    }
+}

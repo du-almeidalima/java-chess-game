8c7c2e8b449afb5568448eba78244a3f0959846d
Xenosgrilda <dualmeidalima@gmail.com>
2019-03-18 19:59:51 -0300

    Implementing possible Knight moves

--- src/models/chess/pieces/Knight.java
+++ src/models/chess/pieces/Knight.java
@@ -3,23 +3,20 @@ package models.chess.pieces;
 import models.boardgame.Board;
 import models.boardgame.Position;
 import models.chess.ChessPiece;
-import models.chess.ChessPosition;
 import models.chess.Color;
 
-import java.util.zip.CheckedInputStream;
-
-public final class King extends ChessPiece{
-    public King(Board board, Color color){
+public final class Knight extends ChessPiece {
+    public Knight(Board board, Color color){
         super(board, color);
     }
 
     // Methods
-    // Checking if king has a allied
     public boolean canMove(Position pos){
         ChessPiece posPiece = (ChessPiece) getBoard().piece(pos);
 
         return (posPiece == null) || (posPiece.getColor() != this.getColor());
     }
+
 
     @Override
     public String toString() {
@@ -28,51 +25,45 @@ public final class King extends ChessPiece{
 
     @Override
     public boolean[][] possibleMoves() {
-        Board board = getBoard();
+        Board board = this.getBoard();
         Position auxPos = new Position(0,0);
         boolean[][] mat = new boolean[board.getRows()][board.getCols()];
 
-        // Checking up
-        auxPos.setValues(this.pos.getRow() - 1, this.pos.getCol());
+        // The Knight moving logic is almost the same as the king, all directions moving 2 positions and turning left/right
+        auxPos.setValues(this.pos.getRow() - 1, this.pos.getCol() - 2);
         if (getBoard().positionExists(auxPos) && this.canMove(auxPos)){
             mat[auxPos.getRow()][auxPos.getCol()] = true;
         }
-        // Checking right-up diagonal
-        auxPos.setValues(this.pos.getRow() - 1, this.pos.getCol() + 1);
+        auxPos.setValues(this.pos.getRow() - 2, this.pos.getCol() - 1);
         if (getBoard().positionExists(auxPos) && this.canMove(auxPos)){
             mat[auxPos.getRow()][auxPos.getCol()] = true;
         }
-        // Checking right
-        auxPos.setValues(this.pos.getRow(), this.pos.getCol() + 1);
+        auxPos.setValues(this.pos.getRow() - 2, this.pos.getCol() + 1);
         if (getBoard().positionExists(auxPos) && this.canMove(auxPos)){
             mat[auxPos.getRow()][auxPos.getCol()] = true;
         }
-        // Checking right-down diagonal
-        auxPos.setValues(this.pos.getRow() + 1, this.pos.getCol() + 1);
+        auxPos.setValues(this.pos.getRow() - 1, this.pos.getCol() + 2);
         if (getBoard().positionExists(auxPos) && this.canMove(auxPos)){
             mat[auxPos.getRow()][auxPos.getCol()] = true;
         }
-        // Checking down
-        auxPos.setValues(this.pos.getRow() + 1, this.pos.getCol());
+        auxPos.setValues(this.pos.getRow() + 1, this.pos.getCol() + 2);
         if (getBoard().positionExists(auxPos) && this.canMove(auxPos)){
             mat[auxPos.getRow()][auxPos.getCol()] = true;
         }
-        // Checking left-down diagonal
-        auxPos.setValues(this.pos.getRow() + 1, this.pos.getCol() - 1);
+        auxPos.setValues(this.pos.getRow() + 2, this.pos.getCol() + 1);
         if (getBoard().positionExists(auxPos) && this.canMove(auxPos)){
             mat[auxPos.getRow()][auxPos.getCol()] = true;
         }
-        // Checking left
-        auxPos.setValues(this.pos.getRow(), this.pos.getCol() - 1);
+        auxPos.setValues(this.pos.getRow() + 2, this.pos.getCol() - 1);
         if (getBoard().positionExists(auxPos) && this.canMove(auxPos)){
             mat[auxPos.getRow()][auxPos.getCol()] = true;
         }
-        // Checking left-up diagonal
-        auxPos.setValues(this.pos.getRow() - 1, this.pos.getCol() - 1);
+        auxPos.setValues(this.pos.getRow() + 1, this.pos.getCol() - 2);
         if (getBoard().positionExists(auxPos) && this.canMove(auxPos)){
             mat[auxPos.getRow()][auxPos.getCol()] = true;
         }
 
         return mat;
     }
+
 }
